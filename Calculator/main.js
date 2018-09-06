495e3b23bdcabcb6f381cf7edf924d4d0765a463
Jesira Sultana Ruhi <30725430+jesira@users.noreply.github.com>
2018-09-06 21:07:46 +0600

    Add files via upload
    basic calculator

---
+++ Calculator/main.js
@@ -0,0 +1,23 @@
+
+
+function calc() {
+    let a = parseInt(document.getElementById('value1').value);
+    let b = parseInt(document.getElementById('value2').value);
+    let op = document.getElementById('operator').value;
+
+    let calculate;
+
+    if(op=== "add"){
+        calculate = a+b;
+    }
+    else if(op=== "min"){
+        calculate = a-b;
+    }
+    else if(op=== "div"){
+        calculate = a/b;
+    }
+    else if(op=== "mul"){
+        calculate = a*b;
+    }
+    document.getElementById('result').innerText = calculate;
+}
